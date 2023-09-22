e5734f8317703f529af66feced98fbdaeeb00674
TAlhwayek <alhwat2@unlv.nevada.edu>
2023-09-21 20:05:23 -0700

    Minor code clean up

--- Dicee/Dicee-iOS13/ViewController.swift
+++ Dicee/Dicee-iOS13/ViewController.swift
@@ -13,15 +13,10 @@ class ViewController: UIViewController {
     @IBOutlet weak var diceImageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
     
-   
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // WHO         WHAT    VALUE
-        //diceImageView1.image = #imageLiteral(resourceName: "DiceSix")
-        //diceImageView2.image = #imageLiteral(resourceName: "DiceTwo")
     }
+    
     @IBAction func rollButtonPressed(_ sender: UIButton) {
         
         let diceArray = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
@@ -29,9 +24,6 @@ class ViewController: UIViewController {
         // Both randomizers do the same thing. Used both to learn.
         diceImageView1.image = diceArray.randomElement()
         diceImageView2.image = diceArray[Int.random(in: 0...5)]
-        
     }
-    
-
 }
 
