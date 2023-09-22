0e9e3c7e383c658612bbcc90154df9035c66f50b
TAlhwayek <alhwat2@unlv.nevada.edu>
2023-09-22 11:10:45 -0700

    Questions and choices are now displayed

--- Destini/Destini-iOS13/Controller/ViewController.swift
+++ Destini/Destini-iOS13/Controller/ViewController.swift
@@ -14,11 +14,31 @@ class ViewController: UIViewController {
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
     
+    let story = StoryBrain()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        cleanUpUI()
+        updateUI()
     }
 
-
+    func updateUI() {
+        storyLabel.text = story.getStoryTitle()
+        choice1Button.setTitle(story.getChoice1(), for: .normal)
+        choice2Button.setTitle(story.getChoice2(), for: .normal)
+    }
+    
+    @IBAction func choiceMade(_ sender: UIButton) {
+        
+    }
+    
+    // Simple function to properly align the text
+    func cleanUpUI() {
+        storyLabel.textAlignment = .center
+        choice1Button.titleLabel?.textAlignment = .center
+        choice2Button.titleLabel?.textAlignment = .center
+    }
+    
 }
 
