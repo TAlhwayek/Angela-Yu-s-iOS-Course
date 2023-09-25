cacbd1f53071b38331bdf0b9979b8b5f7704a200
TAlhwayek <alhwat2@unlv.nevada.edu>
2023-09-24 22:46:32 -0700

    First commit - Project created

---
+++ BMI-Calculator/BMI Calculator/Controllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BMI Calculator
+//
+//  Created by Tony Alhwayek on 24/09/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
