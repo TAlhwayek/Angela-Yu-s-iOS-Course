6a1b770ec82840a5091bec11700a0697346fcc9a
TAlhwayek <alhwat2@unlv.nevada.edu>
2023-09-28 22:55:56 -0700

    Code clean up + added alert
    The alert is for when a user does not enter a total amount, and I modified some of the code to look better

--- Tipsy/Tipsy/ViewController.swift
+++ Tipsy/Tipsy/ViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class MainViewController: UIViewController {
-
+    
     @IBOutlet var totalInput: UITextField!
     
     // Tip buttons
@@ -24,52 +24,60 @@ class MainViewController: UIViewController {
         super.viewDidLoad()
         
     }
-
-
+    
+    
     @IBAction func tipChanged(_ sender: UIButton) {
         // Dismiss keyboard
         totalInput.endEditing(true)
         
+        // Deselect all tip buttons
         zeroPctButton.isSelected = false
         tenPctButton.isSelected = false
         twentyPctButton.isSelected = false
         
+        // Highlight seleceted tip button
         sender.isSelected = true
         
         var percent = sender.currentTitle!
         percent = String(percent.dropLast())
         
-        tipAmount = Double(percent)!
-        tipAmount = tipAmount / 100
+        // Get tip amount
+        if let percentString = sender.currentTitle?.dropLast(), let percent = Double(percentString) {
+            tipAmount = percent / 100
+        }
     }
     
     @IBAction func stepperChanged(_ sender: UIStepper) {
         // Dismiss keyboard
         totalInput.endEditing(true)
         // Get value from stepper and update label
-        splitLabel.text = String(Int(sender.value))
+        splitLabel.text = "\(Int(sender.value))"
     }
     
     @IBAction func calculatePressed(_ sender: UIButton) {
-        var total = 0.0
-        let split = Double(splitLabel.text!)
-        
-        // Check for empty total amount
-        if totalInput.text != "" {
-            total = Double(totalInput.text!)!
+        // Check for total amount
+        // Alert user if total is empty/blank
+        guard let totalString = totalInput.text, let total = Double(totalString) else {
+            // Defined and present alert
+            let emptyInputAC = UIAlertController(title: "Please enter a total", message: "You cannot split an empty bill!", preferredStyle: .alert)
+            let okButton = UIAlertAction(title: "OK", style: .default)
+            emptyInputAC.addAction(okButton)
+            present(emptyInputAC, animated: true)
+            // Return and do not calculate
+            return
         }
+        
+        let split = Double(splitLabel.text ?? "2")
         
         // Calculate total per person and save as a string
         let totalPerPerson = String(format: "%.2f", (total + (total * tipAmount)) / split!)
-        print(totalPerPerson)
         
         // Present next VC
         if let finalVC = storyboard?.instantiateViewController(withIdentifier: "FinalVC") as? FinalViewController {
             finalVC.total = totalPerPerson
             finalVC.people = Int(split!)
-            var tipPct = String(Int(tipAmount * 100))
-            tipPct = tipPct + "%"
-            finalVC.tipPct = tipPct
+            let tipPct = Int(tipAmount * 100)
+            finalVC.tipPct = "\(tipPct)%"
             present(finalVC, animated: true)
         }
         
