1be3005e72fbc1400d3cd57e3509701019eae1e8
TAlhwayek <alhwat2@unlv.nevada.edu>
2023-09-28 21:55:18 -0700

    First commit

---
+++ Tipsy/Tipsy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tipsy
+//
+//  Created by Tony Alhwayek on 09/28/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
