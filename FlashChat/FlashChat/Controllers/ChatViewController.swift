45ad62d6e703023124debe4fc4bf9e69a16c1c13
TAlhwayek <alhwat2@unlv.nevada.edu>
2023-10-19 23:26:37 -0700

    Added logout button + functionality on chat screen

--- FlashChat/FlashChat/Controllers/ChatViewController.swift
+++ FlashChat/FlashChat/Controllers/ChatViewController.swift
@@ -6,20 +6,38 @@
 //  Copyright © 2019 Angela Yu. All rights reserved.
 //
 
+import FirebaseAuth
+import FirebaseCore
 import UIKit
 
 class ChatViewController: UIViewController {
-
+    
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var messageTextfield: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        title = "⚡️FlashChat"
+        // Add a logout button
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Logout", style: .plain, target: self, action: #selector(logoutUser))
+        // Hide back button because it feels useless in a chat app like this
+        navigationItem.hidesBackButton = true
+        
     }
     
     @IBAction func sendPressed(_ sender: UIButton) {
     }
     
-
+    // When logout button is pressed
+    @objc func logoutUser() {
+        do {
+            // Try logging out
+            try Auth.auth().signOut()
+            // Go back to starting view controller
+            navigationController?.popToRootViewController(animated: true)
+        } catch let signOutError as NSError {
+            print("Error signing out: %@", signOutError)
+        }
+    }
 }
